dd921a50b05f608db75908e020d3702319750c12
Rudnei Oliveira <rhoodsk8erboy@gmail.com>
2024-07-21 19:40:16 +0100

    Create: update methods of update and delete genre

--- NOS.Engineering.Challenge.API/Controllers/ContentController.cs
+++ NOS.Engineering.Challenge.API/Controllers/ContentController.cs
@@ -1,20 +1,24 @@
+using System.Collections;
+using System.Linq;
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using NOS.Engineering.Challenge.API.Models;
+using NOS.Engineering.Challenge.Database;
 using NOS.Engineering.Challenge.Managers;
+using NOS.Engineering.Challenge.Models;
 
 namespace NOS.Engineering.Challenge.API.Controllers;
 
 [Route("api/v1/[controller]")]
 [ApiController]
-public class ContentController : Controller
+public class ContentController : ControllerBase
 {
     private readonly IContentsManager _manager;
     public ContentController(IContentsManager manager)
     {
         _manager = manager;
     }
-    
+
     [HttpGet]
     public async Task<IActionResult> GetManyContents()
     {
@@ -22,7 +26,7 @@ public class ContentController : Controller
 
         if (!contents.Any())
             return NotFound();
-        
+
         return Ok(contents);
     }
 
@@ -33,10 +37,10 @@ public class ContentController : Controller
 
         if (content == null)
             return NotFound();
-        
+
         return Ok(content);
     }
-    
+
     [HttpPost]
     public async Task<IActionResult> CreateContent(
         [FromBody] ContentInput content
@@ -46,7 +50,7 @@ public class ContentController : Controller
 
         return createdContent == null ? Problem() : Ok(createdContent);
     }
-    
+
     [HttpPatch("{id}")]
     public async Task<IActionResult> UpdateContent(
         Guid id,
@@ -57,7 +61,7 @@ public class ContentController : Controller
 
         return updatedContent == null ? NotFound() : Ok(updatedContent);
     }
-    
+
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteContent(
         Guid id
@@ -66,22 +70,84 @@ public class ContentController : Controller
         var deletedId = await _manager.DeleteContent(id).ConfigureAwait(false);
         return Ok(deletedId);
     }
-    
+
     [HttpPost("{id}/genre")]
-    public Task<IActionResult> AddGenres(
+    public async Task<IActionResult> AddGenres(
         Guid id,
         [FromBody] IEnumerable<string> genre
     )
     {
-        return Task.FromResult<IActionResult>(StatusCode((int)HttpStatusCode.NotImplemented));
+
+        var content = await _manager.GetContent(id).ConfigureAwait(false);
+
+        if (content == null || genre == null)
+        {
+            Console.WriteLine("passei aqui");
+            return NotFound();
+        }
+
+        List<string> genres = content.GenreList.ToList();
+        List<string> genreList = genre.ToList();
+        List<string> filteredItems = genreList.Where(x => genres.All(y => !y.Equals(x))).ToList();
+
+        if (filteredItems.Count == 0)
+        {
+            return NotFound();
+        }
+
+        foreach (var item in filteredItems)
+        {
+            genres.Add(item);
+        }
+
+        ContentDto newData = new(
+            content.Title,
+            content.SubTitle,
+            content.Description,
+            content.ImageUrl,
+            content.Duration,
+            content.StartTime,
+            content.EndTime,
+            genres
+        );
+
+        var updatedContent = await _manager.UpdateContent(id, newData).ConfigureAwait(false);
+
+        return updatedContent == null ? NotFound() : Ok(updatedContent);
     }
-    
+
     [HttpDelete("{id}/genre")]
-    public Task<IActionResult> RemoveGenres(
+    public async Task<IActionResult> RemoveGenres(
         Guid id,
         [FromBody] IEnumerable<string> genre
     )
     {
-        return Task.FromResult<IActionResult>(StatusCode((int)HttpStatusCode.NotImplemented));
+
+        var content = await _manager.GetContent(id).ConfigureAwait(false);
+
+        if (content == null || genre == null)
+        {
+            return NotFound();
+        }
+
+
+        List<string> genres = content.GenreList.ToList();
+        List<string> genreList = genre.ToList();
+        List<string> filteredItems = genres.Where(x => genreList.All(y => !y.Equals(x))).ToList();
+
+        ContentDto data = new(
+            content.Title,
+            content.SubTitle,
+            content.Description,
+            content.ImageUrl,
+            content.Duration,
+            content.StartTime,
+            content.EndTime,
+            filteredItems
+        );
+
+        var updatedContent = await _manager.UpdateContent(id, data).ConfigureAwait(false);
+
+        return updatedContent == null ? NotFound() : Ok(updatedContent);
     }
 }
